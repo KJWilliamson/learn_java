bdf1094a1637998a9efb0f031e4c7018c1f1ae6c
kamela williamson <kami63116@yahoo.com>
2024-03-18 15:48:45 -0700

    first commit

---
+++ Program.java
@@ -0,0 +1,20 @@
+public class Program
+{
+    public static void main(String[] args) {
+
+        
+        int tempFahrenheit = 70;
+          if (tempFahrenheit < 32){
+            System.out.println("Must be freezing!");
+          } else if (tempFahrenheit > 80) {
+            System.out.println("Getting hot!");
+          } else {
+            System.out.println("Seems like a nice day!");
+          }
+
+
+
+		
+	}
+}
+
