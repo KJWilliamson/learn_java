ac1ce3513789f702c38eebc43c7152f4e40d05dc
Kamela <kami63116@yahoo.com>
2024-03-18 16:30:18 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -4,9 +4,9 @@ public class Program
 
         
         int tempFahrenheit = 70;
-          if (tempFahrenheit < 32){
+          if (tempFahrenheit < 55){
             System.out.println("Must be freezing!");
-          } else if (tempFahrenheit > 80) {
+          } else if (tempFahrenheit > 99) {
             System.out.println("Getting hot!");
           } else {
             System.out.println("Seems like a nice day!");
